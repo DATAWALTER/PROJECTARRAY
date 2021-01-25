4e9200996874bfd9dba10846a57fd0f0ce06169f
Walter <sagastegui@hotmail.es>
2021-01-25 10:00:40 +0100

    1 SNAPSHOT Main1.java

---
+++ src/misrc/Main1.java
@@ -0,0 +1,14 @@
+package misrc;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        //DECLARACION ARRAY
+        String[] nombre = {"Luis","Miguel","Carlos"};
+        //RECORRER EL ARRAY
+        for(int i=0; i<nombre.length; i++) {
+            System.out.println(nombre[i]);
+        }
+    }
+    
+}
