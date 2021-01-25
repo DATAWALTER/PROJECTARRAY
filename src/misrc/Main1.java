e6cef18e5e3f37967872eb1e7a7bb2998dbb4b0b
Walter <sagastegui@hotmail.es>
2021-01-25 10:06:09 +0100

    2 SNAPSHOT Main1.java array double

--- src/misrc/Main1.java
+++ src/misrc/Main1.java
@@ -3,12 +3,17 @@ package misrc;
 public class Main1 {
 
     public static void main(String[] args) {
-        //DECLARACION ARRAY
+        //DECLARACION ARRAY TIPO CADENA
         String[] nombre = {"Luis","Miguel","Carlos"};
         //RECORRER EL ARRAY
         for(int i=0; i<nombre.length; i++) {
             System.out.println(nombre[i]);
         }
+        //DECLARACION ARRAY TIPO DOUBLE
+        double[] estatura = {1.72,1.75,1.65};
+        //RECORRER EL ARRAY
+        for(int i=0; i<estatura.length; i++) {
+            System.out.println(estatura[i]);
+        }
     }
-    
 }
